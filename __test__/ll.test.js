8bde08d57ee181f0c95a17441102328b4dbea052
fha96 <fahadalrimawe96@hotmail.com>
2022-09-09 14:42:50 +0300

    complete testing for insert && append

--- __test__/ll.test.js
+++ __test__/ll.test.js
@@ -14,12 +14,44 @@ describe('testing linked list !', () => {
         expect(ll.head).toBeNull();
     });
 
-    it('test add a node to first of an empty linked list', () => {
+    it('test add a node to the first of an empty linked list', () => {
         const ll = new LinkedList();
         ll.insert('fahad');
         expect(ll).toBeInstanceOf(LinkedList);
         expect(ll.head).not.toBeNull();
         expect(ll.head.value).toEqual('fahad');
         expect(ll.head.next).toBeNull();
+    });
+
+    it('test add a node to the first of non-empty linked list', () => {
+        const ll = new LinkedList();
+        ll.insert('fahad');
+        ll.insert('ahmd');
+        expect(ll).toBeInstanceOf(LinkedList);
+        expect(ll.head).not.toBeNull();
+        expect(ll.head.value).toEqual('ahmd');
+        expect(ll.head.next.value).toEqual('fahad');
+        expect(ll.head.next.next).toBeNull();
+    });
+
+    it('test add a node to the end to an empty linked list ', () =>{
+        const ll = new LinkedList();
+        ll.append('fahad');
+        expect(ll).toBeInstanceOf(LinkedList);
+        expect(ll.head.next).toBeNull();
+        expect(ll.head.value).toEqual('fahad');
+    });
+
+    it('test add a node to the end of non-empty linked list',() => {
+        const ll = new LinkedList();
+        ll.append('fahad');
+        ll.append('ahmd');
+        ll.append('anas');
+        expect(ll).toBeInstanceOf(LinkedList);
+        expect(ll.head.value).toEqual('fahad');
+        expect(ll.head.next.value).toEqual('ahmd');
+        expect(ll.head.next.next.value).toEqual('anas');
+        expect(ll.head.next.next.next).toBeNull();
+    
     });
 });
